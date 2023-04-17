afded364f32051bdfaff5e606c29d5d574f8cf1b
Arindam Mukherjee <60281054+ArindamMukherjee@users.noreply.github.com>
2023-04-17 09:00:19 +0000

    added project.html page with some styling

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template
 
+Projects=[
+  {
+     'id': 1,
+     'title' : "Autonomious Drone",
+  },
+
+  {
+     'id': 2,
+     'title' : "Obstacle avoider using Ultrasonic Sensor",
+  },
+
+  {
+     'id': 3,
+     'title' : "VRV",
+  }
+]
+
 app = Flask(__name__) # creat an object 
 
 @app.route("/")  # route the app
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html",projects=Projects)
+
+# @app.route("/projects"):
+# def projects():
+#   return 
 if __name__ =="__main__":
   app.run(host = "0.0.0.0",port = "5000", debug = True) # 
